115a2d4e61102fe03be0881eb37fbd672ff6a8b3
Syrine <skhammari@aisuperior.com>
2023-09-28 14:51:14 +0000

    adjust region_propose outputs

--- pyaugment/modules/region_proposer/seem_region_proposer.py
+++ pyaugment/modules/region_proposer/seem_region_proposer.py
@@ -1,10 +1,19 @@
+import os
+import sys
 from pathlib import Path
 from typing import List
+
+## TODO: find a better solution for this
+sys.path.append("/home/ubuntu/Segment-Everything-Everywhere-All-At-Once/demo_code")
 
 import numpy as np
 import torch
 from PIL import Image
 from supervision.detection.core import Detections
+from tasks import interactive_infer_image
+from utils.arguments import load_opt_from_config_files
+from utils.constants import COCO_PANOPTIC_CLASSES
+from utils.distributed import init_distributed
 from xdecoder import build_model
 from xdecoder.BaseModel import BaseModel
 
@@ -14,9 +23,9 @@ from pyaugment.modules.region_proposer.base_region_proposer import (
 )
 
 
-class GroundedSAMRegionProposer(BaseRegionProposer):
+class SEEMRegionProposer(BaseRegionProposer):
     def __init__(self, config_file_path: str) -> None:
-        opt = load_opt_from_config_files(conf_files)
+        opt = load_opt_from_config_files(config_file_path)
         opt = init_distributed(opt)
         if "focalt" in config_file_path:
             pretrained_pth = Path("seem_focalt_v2.pt")
@@ -42,25 +51,22 @@ class GroundedSAMRegionProposer(BaseRegionProposer):
         prompt = [c + "." for c in prompt]
 
         image_list = Path(images_path).glob("*")
-
         annotated_images = []
 
         for image in image_list:
-            image_path = Path(images_path, image)
-            detections = self._get_detections(image_path, prompt)
-            annotated_image = AnnotatedImage(
-                images_path=image_path, detections=detections
-            )
+            detections = self._get_detections(image, prompt)
+            annotated_image = AnnotatedImage(file_name=image, detections=detections)
             annotated_images.append(annotated_image)
 
         return annotated_images
 
     def _get_detections(self, image_path: str, prompt: List[str]) -> Detections:
-        input = {"image": Image.open(image_path), "mask": None}
-        masks = np.empty_like([])
-        classes = np.empty_like([])
+        image = Image.open(image_path)
+        input = {"image": image, "mask": None}
+        masks = np.empty(shape=(len(prompt), image.size[0], image.size[1]))
+        classes = np.empty(shape=(len(prompt)))
 
-        for cat in prompt:
+        for i, cat in enumerate(prompt):
             output = interactive_infer_image(
                 model=self.model,
                 audio_model=None,
@@ -68,14 +74,14 @@ class GroundedSAMRegionProposer(BaseRegionProposer):
                 tasks=["Text"],
                 reftxt=cat,
             )
-            class_id = self.object_categories[prompt[:-1]]
-            masks = np.append(masks, output)
-            classes = np.append(classes, class_id)
-
+            output = output[0].resize(image.size)
+            class_id = self.object_categories[cat[:-1]]
+            masks[i, :, :] = np.array(output)[:, :, 0]
+            classes[i] = class_id
         xyxy = np.zeros((len(prompt), 4))
         detections = Detections(
             xyxy=xyxy,
-            mask=masks,
+            mask=masks.astype(np.uint8),
             class_id=classes,
         )
 
