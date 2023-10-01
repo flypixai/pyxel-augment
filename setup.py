5e60701204635e6cfa4bf12b190339f2b01f3992
Ubuntu <skhammari@aisuperior.com>
2023-10-01 10:15:02 +0000

    add grounding SAM and SEEM dependencies

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
-  
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='pyaugment',
-    version='0.1',
-    description='A package for automated object based data augmentation',
-    author='Syrine Khammari',
-    author_email='skhammari@aisupeiror.com',
-    packages=['pyaugment'],
-    install_requires=[
-        'numpy',
-        'pandas',
-    ],
+    name="pyaugment",
+    version="0.1",
+    description="A package for automated object based data augmentation",
+    author="Syrine Khammari",
+    author_email="skhammari@aisupeiror.com",
+    packages=["pyaugment"],
+    install_requires=requirements,
 )
