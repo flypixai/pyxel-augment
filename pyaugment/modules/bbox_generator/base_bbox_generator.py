860884fff15bc69297d507b7eb0f8984f4ee1d4d
Ubuntu <skhammari@aisuperior.com>
2023-10-16 18:30:13 +0000

    add possibility to save added objects as yolo format

--- pyaugment/modules/bbox_generator/base_bbox_generator.py
+++ pyaugment/modules/bbox_generator/base_bbox_generator.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from pathlib import Path
 from typing import List, Optional
 
 import numpy
@@ -24,3 +25,13 @@ class BaseRBBoxGenerator(ABC):
         self, proposed_regions: List[AnnotatedImage], object_size: ObjectSize
     ) -> List[RBBox]:
         pass
+
+    def save_as_yolo(self, obj_id, bboxes: List[RBBox], images: List[AnnotatedImage]):
+        for i, bbox in enumerate(bboxes):
+            image_name = images[i].file_name
+            file_name = str(image_name.name)[:-3] + "txt"
+            file_path = Path(image_name.parent.parent, "labels", file_name)
+            bbox_in_image = bbox
+            yolo_annotation = f"{obj_id} {bbox_in_image.x_center} {bbox_in_image.y_center} {bbox_in_image.width} {bbox_in_image.height} {bbox_in_image.alpha} \n"
+            with open(file_path, "a") as text_file:
+                text_file.write(yolo_annotation)
